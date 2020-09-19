582d7705c19f3da7762bf202146751ea3b921412
Natalie <natalie.ramer@ou.edu>
2020-09-18 19:25:57 -0500

    Initial Commit

---
+++ R/myf.R
@@ -0,0 +1,15 @@
+#' Square Function
+#'
+#' Takes a vector and returns a vector of squared components
+#'
+#' @param x
+#'
+#' @return a vector of squares
+#' @export
+#'
+#' @examples
+#' x=(1:30);myf(x)
+#'
+myf=function(x){
+  x^2
+}
